71dd12a233571ec66e229392b27c3285e5d4180d
Abhee Chaudhary <abheeac.2508@gmail.com>
2026-05-26 00:30:35 +0530

    Start Work on SPSC. Add README

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,25 @@
+#include <functional>
 #include <iostream>
 #include "memory/arena.h"
+#include <thread>
+#include <vector>
+
+int shared_counter = 0;
+
+void increment(){
+    for (int i = 0; i < 10000000;i++) shared_counter++;
+}
 
 
 int main() {
-
-    Arena my_arena(1024); // on stack for fast access and auto destruction
-
-    std::cout << my_arena.alloc(1,1) << std::endl;
-    std::cout << my_arena.alloc(4,4) << std::endl;
-    std::cout << my_arena.alloc(8,8) << std::endl;
-
-
-    //
-    // int x {5};// on stack
-    // int* y = (int*) malloc( sizeof(int)); // on heap
-    // *y = 10;
-
-
-// ARENA ALLOCATOR
+    const auto timer = std::chrono::high_resolution_clock::now();
+    std::thread t1(increment);
+    std::thread t2(increment);
+    t1.join();
+    t2.join();
+    std::cout << shared_counter << std::endl;
+    const auto end_time = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - timer).count() << " ms" << std::endl;
 
 
 
