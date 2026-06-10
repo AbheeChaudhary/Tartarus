57ae8605a372d9cb93a2763d238c80f1533ed6a8
Abhee Chaudhary <abheeac.2508@gmail.com>
2026-06-10 13:23:45 +0530

    Implement RingBuffer class with push and pop methods for efficient data handling

--- src/concurrency/RingBuffer.h
+++ src/concurrency/RingBuffer.h
@@ -11,9 +11,42 @@
 
 
 template <typename T> class RingBuffer {
-    std::Array<T> buffer;
-private :
+
+private:
+    T* buffer;
     std::size_t capacity;
-    std::atomic<std::size_t> head;
-    std::atomic<std::size_t> tail;
+    alignas(64) std::atomic<std::size_t> head;
+    alignas(64) std::atomic<std::size_t> tail;
+
+public:
+    explicit RingBuffer(const std::size_t size) {
+        capacity = size+1; // 1 empty slot to diff between full buffer and empty
+        head.store(0);
+        tail.store(0);
+        buffer = new T[capacity];
+    }
+
+    ~RingBuffer() {
+        delete[] buffer;
+    }
+
+    bool push(const T& item) {
+
+        std::size_t current_head = head.load(std::memory_order_relaxed);
+        const std::size_t next_head = (current_head  + 1) % capacity; // wrap around
+        if (const std::size_t current_tail = tail.load(std::memory_order_acquire); next_head == current_tail) return false; // buffer is full
+        buffer[current_head] = item;
+        head.store(next_head,std::memory_order_release);
+        return true;
+
+    }
+    bool pop(T& item) {
+        std::size_t current_tail = tail.load(std::memory_order_relaxed);
+        if (const std::size_t current_head = head.load(std::memory_order_acquire); current_tail == current_head) return false;
+        item = buffer[current_tail];
+        const std::size_t next_tail = (current_tail + 1) % capacity;
+        tail.store(next_tail,std::memory_order_release);
+        return true;
+    }
+
 };
