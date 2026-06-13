62efde13984b31669ecc7e52a70ecb75dfa4d872
Abhee Chaudhary <abheeac.2508@gmail.com>
2026-06-14 02:41:43 +0530

    Add template Arena Allocator wrapper class which follows allocator boilerplate to convert Arena into allocator_argument

--- src/memory/arena.h
+++ src/memory/arena.h
@@ -22,5 +22,40 @@ public :
 };
 
 
+template <typename T>
+class ArenaAllocator {
+public :
+    using value_type = T;
+    Arena* arena;
+
+    ArenaAllocator(Arena* a) : arena(a){}
+
+    template <typename U>
+    ArenaAllocator(const ArenaAllocator<U>& other) noexcept : arena(other.arena){}
+
+    T* allocate (std::size_t n) {
+        void* p =  arena->alloc(n*sizeof(T),alignof(T));
+        if (!p) throw std::bad_alloc();
+        return static_cast<T*>(p);
+    }
+
+    void deallocate(T* p, std::size_t n) noexcept {
+        // doesn't do as when the arena is destroyed everything is destroyed
+    }
+
+    template <typename U>
+    struct rebind{ using other = ArenaAllocator<U>; };
+
+};
+
+
+// Defining / overloading operators
+
+template <typename T, typename U>
+bool operator == (const ArenaAllocator<T>& a, const ArenaAllocator<U>& b) noexcept { return a.arena == b.arena; }
+template <typename T, typename U>
+bool operator != (const ArenaAllocator<T>& a, const ArenaAllocator<U>& b) noexcept { return !(a == b); }
+
+
 
 
